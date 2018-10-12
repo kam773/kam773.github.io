7d80b1df320f2b59f54ee084a7ea040075f4dc07
Kamil <developer.kamil90@gmail.com>
2018-10-12 18:00:26 +0200

    add build

--- gratia/gulpfile.js
+++ gratia/gulpfile.js
@@ -2,6 +2,14 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var changed = require('gulp-changed');
+var htmlReplace = require('gulp-html-replace');
+var htmlMin = require('gulp-htmlmin');
+var del = require('del');
+var sequence = require('run-sequence');
 
 
 // Server
@@ -23,6 +31,49 @@ gulp.task('sass', function() {
                }))
                .pipe(gulp.dest('src/css'))
                .pipe(browserSync.stream());
+});
+
+gulp.task('css', function() {
+    return gulp.src('src/css/**/*.css')
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('dist/css'));
+
+});
+
+gulp.task('js', function() {
+    return gulp.src('src/js/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+
+});
+
+gulp.task('img', function() {
+    return gulp.src('src/img/**/*.{jpg,jpeg,png,gif}')
+        .pipe(changed('dist/img'))
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+
+});
+
+gulp.task('html', function() {
+    return gulp.src('src/*.html')
+        .pipe(htmlReplace({
+            'css': 'css/main.css',
+            'js': 'js/script.js'
+        }))
+        .pipe(htmlMin({
+            sortAttributes: true,
+            sortClassName: true
+        }))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('clean', function() {
+    return del(['dist'])
+});
+
+gulp.task('build', function() {
+    sequence('clean', ['html', 'js', 'css', 'img']);
 });
 
 gulp.task('default', ['serve']);

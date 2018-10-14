1859b434993d9b2d0ab36be7b0745068d89343f6
Kamil <developer.kamil90@gmail.com>
2018-10-14 14:43:03 +0200

    add bags

--- bags-world/README.md
+++
@@ -1,1 +0,0 @@
-# bag-shop-website
---
+++ bags-world/gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
+
+// Server
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: 'src'
+    });
+
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch('src/sass/**/*.scss', ['sass']);
+});
+
+// Compile SASS to CSS
+gulp.task('sass', function() {
+    return gulp.src('src/sass/**/*.scss')
+               .pipe(sass().on('error', sass.logError))
+               .pipe(autoprefixer({
+                browsers: ['last 3 versions']
+               }))
+               .pipe(gulp.dest('src/css'))
+               .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);

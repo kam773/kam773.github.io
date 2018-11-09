b5a32920d6d830bb4a5bd5e8ee020a6af1c65445
Kamil <developer.kamil90@gmail.com>
2018-11-09 22:36:06 +0100

    add office

--- tax-office/src/js/script.js
+++ tax-office/src/js/script.js
@@ -1,13 +1,5 @@
 
 
-function initMap() {
-  
-    var uluru = {lat: 53.6944002, lng: 17.5569252};
-  
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
-        
-    var marker = new google.maps.Marker({position: uluru, map: map});
-  }
-  
+
+
 

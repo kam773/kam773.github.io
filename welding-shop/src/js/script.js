4f2eaf8cd7056660c55ef828df960ba21ad336b6
Kamil <developer.kamil90@gmail.com>
2018-11-21 14:06:55 +0100

    add

--- welding-shop/src/js/script.js
+++ welding-shop/src/js/script.js
@@ -30,4 +30,13 @@ $(document).ready(function() {
         autoplaySpeed: 2000
         
     });
+
+    $('.brand-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000
+        
+    });
+
 });

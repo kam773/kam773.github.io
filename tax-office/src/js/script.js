dd75b8f28820dff0c912643f293104051dcd2eae
Kamil <developer.kamil90@gmail.com>
2019-01-20 19:06:36 +0100

    add

--- tax-office/src/js/script.js
+++ tax-office/src/js/script.js
@@ -20,43 +20,44 @@ $('.scrollToTop').click(function() {
     }, 500);
 });
 
+
 $(document).ready(function(){
-    $(".hamburger").click(function(){
-        $(".mobile").slideToggle(500);
-    });
-});
-
 /* ScrollTo*/
-$('a[href*="#"]')
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
+    $('a[href*="#"]')
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
 
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      &&
-      location.hostname == this.hostname
-    ) {
+        if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+        ) {
 
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 
-      if (target.length) {
+        if (target.length) {
 
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top - $('.navigation').height()
-        }, 1000, function() {
+            event.preventDefault();
+            $('html, body').animate({
+            scrollTop: target.offset().top - $('.navigation').height()
+            }, 1000, function() {
 
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) {
-            return false;
-          } else {
-            $target.attr('tabindex','-1');
+            var $target = $(target);
             $target.focus();
-          };
-        });
-      }
-    }
+            if ($target.is(":focus")) {
+                return false;
+            } else {
+                $target.attr('tabindex','-1');
+                $target.focus();
+            };
+            });
+        }
+        }
+    });
+
+    $('.menu-icon').click(function() {
+        $("ul").toggleClass("active");
+});
 });

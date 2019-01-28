30c738ed6cb5ecef61ae2dfc3e3e75462eddca90
Kamil <developer.kamil90@gmail.com>
2019-01-28 21:19:08 +0100

    add

--- portfolio/src/js/scripts.js
+++ portfolio/src/js/scripts.js
@@ -87,3 +87,31 @@ $(document).ready(function() {
 	}, 3000);
 
 });
+
+$(function () {
+
+    var filterList = {
+
+        init: function () {
+
+            // MixItUp plugin
+            // http://mixitup.io
+            $('#portfoliolist').mixItUp({
+              selectors: {
+              target: '.portfolio',
+              filter: '.filter'
+          },
+          load: {
+            filter: '.app'
+          }
+            });
+
+        }
+
+    };
+
+    // Run the show!
+    filterList.init();
+
+
+});
